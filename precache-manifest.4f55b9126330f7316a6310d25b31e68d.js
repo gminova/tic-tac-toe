de9789de739b9961de6172f66363cee8c01d830c
gminova <minova1995@gmail.com>
2019-08-24 12:51:05 +0100

    Updates

---
+++ precache-manifest.4f55b9126330f7316a6310d25b31e68d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4b08645adad46f4936ae129dc913be",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "57caddaffbb5421ca045",
+    "url": "/tic-tac-toe/static/css/main.a9827d45.chunk.css"
+  },
+  {
+    "revision": "c4cb3f5e1dd21a69d4d2",
+    "url": "/tic-tac-toe/static/js/2.69501f5b.chunk.js"
+  },
+  {
+    "revision": "57caddaffbb5421ca045",
+    "url": "/tic-tac-toe/static/js/main.f63b6e58.chunk.js"
+  },
+  {
+    "revision": "97e640ec0afe7d690a63",
+    "url": "/tic-tac-toe/static/js/runtime~main.f9de6c25.js"
+  }
+]);
